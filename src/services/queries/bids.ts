7ee133afabb1d3586873ed338a826fd6976eb04a
Eryx Guimarães <eryxjose@hotmail.com>
2026-07-24 17:16:04 -0300

    Implement bid creation logic: validate item existence, bid amount, and item status; update bid history and item details in Redis

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,12 +1,35 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
-import { bidHistoryKey } from '$services/keys';
+import { bidHistoryKey, itemsKey } from '$services/keys';
 import { client } from '$services/redis';
 import { DateTime } from 'luxon';
+import { getItem } from '$services/queries/items';
+
 
 export const createBid = async (attrs: CreateBidAttrs) => {
 	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
 
-	return client.rPush(bidHistoryKey(attrs.itemId), serialized);
+	const item = await getItem(attrs.itemId);
+
+	if (!item) {
+		throw new Error(`Item with id ${attrs.itemId} not found`);
+	}
+
+	if (item.price >= attrs.amount) {
+		throw new Error('Bid too low');
+	}
+
+	if (item.endingAt.diff(DateTime.now()).toMillis() <= 0) {
+		throw new Error('Item has already ended');
+	}
+
+	return Promise.all([
+		client.rPush(bidHistoryKey(attrs.itemId), serialized),
+		client.hSet(itemsKey(item.id), {
+			bids: item.bids + 1,
+			price: attrs.amount,
+			highestBidUserId: attrs.userId
+		})
+	]);
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
